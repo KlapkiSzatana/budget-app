f32b31a0397dc818789316b350a1beec8b2f1ad6
KlapkiSzatana <257149538+KlapkiSzatana@users.noreply.github.com>
2026-02-14 21:17:06 +0100

    Podniesienie numerka

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 import gettext
 
 # --- STAŁE APLIKACJI ---
-WERSJA = "0.9.4"
+WERSJA = "0.9.5"
 PRODUCENT = "KlapkiSzatana"
 CASH_SAVINGS_NAME = "Oszczędności gotówka"  # Nazwa techniczna w bazie (nie tłumaczyć klucza)
 
